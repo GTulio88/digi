77eb789732e42f419203ca05763f279e3c866831
Gtulio88 <getuliomreis@hotmail.com>
2025-01-26 22:56:11 +0100

    vite add

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -4,10 +4,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build",
+    outDir: "build", // Gera os arquivos na pasta "build"
   },
   server: {
-    host: "0.0.0.0",
-    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+    host: "0.0.0.0", // Permite conexões externas
+    port: process.env.PORT || 5173, // Usa a porta configurada no ambiente
+    allowedHosts: [
+      "digi-uckg.onrender.com", // Adiciona seu domínio Render
+    ],
   },
 });
