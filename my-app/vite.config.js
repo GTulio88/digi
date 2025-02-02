b720ed2d62b788a1d9d80154dcb8ff7a091126c4
Gtulio88 <getuliomreis@hotmail.com>
2025-02-02 14:14:45 +0100

    fix vite

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -4,24 +4,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build", // Gera os arquivos na pasta "build"
-  },
-  server: {
-    host: "0.0.0.0", // Permite conexões externas
-    port: process.env.PORT || 5173, // Usa a porta configurada no ambiente
-  },
-});
-
-/*import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  build: {
     outDir: "build",
   },
   server: {
     host: "0.0.0.0",
     port: process.env.PORT || 5173,
+    allowedHosts: [
+      "digi-uckg.onrender.com", // Adiciona o domínio do Render
+      "localhost", // Permite o localhost para testes locais
+    ],
   },
-});*/
+});
