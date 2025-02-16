8904e0300fe2ba1555d9e2e048b3d3bfc679cf85
Gtulio88 <getuliomreis@hotmail.com>
2025-02-16 12:18:31 +0100

    fixes

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const mongoose = require("mongoose");
 
 // Criando a instância do aplicativo Express
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 const MONGO_URI = process.env.MONGO_URI;
 
 // Verifica se a variável de ambiente MONGO_URI está definida
